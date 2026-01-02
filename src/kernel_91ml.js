98a1654ca8c4c0120ea7be732c051cd36d512f02
xcvxc3223 <l6i6g06omb@mail10p.com>
2026-01-02 10:42:49 +0700

    docs: update installation guide

--- src/kernel_91ml.js
+++ src/kernel_91ml.js
@@ -3,3 +3,8 @@ module.exports = config;
 
 // Update zrrm
 const service_zrrm = <T>(args: T): T => { return args; };
+
+// Update xvny
+def run_task_xvny():
+    """Subsystem version 3807"""
+    return "xvny".upper()
